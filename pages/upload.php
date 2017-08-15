2630a963f4911d9159ba8f5e19fd17873d459384
unknown <zhaoyang.peng@SWD-ZHAOYANG-D1.cn.ta-mp.com>
2017-08-15 11:20:45 +0800

    remov language array format

--- pages/upload.php
+++ pages/upload.php
@@ -115,26 +115,26 @@ function toggle(link, divId) { var lText = link.innerHTML; var d = getObject(div
 <body onload="denyTerms()">
 <tr><td colspan=2 style="border:1px #AAAAAA solid;height:100%;background-color:#FFFFFF;padding:20px;text-align:left;" valign=top>
   	<?php include("./ads.php"); ?>
-<p><center><?php echo $lang[info];?></center></p>
+<p><center><?php echo $info;?></center></p>
 
-<h1><center><?php echo $lang[upload];?></center></h1>
+<h1><center><?php echo $upload;?></center></h1>
 	<br />
 	<center>
 	<form enctype="multipart/form-data" action="upload.php?do=verify" id="form" method="post" onsubmit="a=document.getElementById('form').style;a.display='none';b=document.getElementById('part2').style;b.display='inline';" style="display: inline;">
-	<?php echo $lang[maxsize];?> <b><?php echo $maxfilesize; ?> MB</b><br />
+	<?php echo $maxsize;?> <b><?php echo $maxfilesize; ?> MB</b><br />
          <table border=0 cellspacing=0 cellpadding=2><tr><td align=center colspan=2>
 	<?php echo $filetypes; ?>
 	<img src="img/bild.gif"> <input type="file" name="upfile" size="50" /></td></tr>
          <tr><td align=left>
-	<?php if($emailoption) { ?><?php echo $lang[emailopt];?>:</td><td align=right><input type="text" name="myemail" size="40" /> <i>(<?php echo $lang[opt];?>)</i></td></tr><?php } ?>
-	<tr><td align=left><?php if($descriptionoption) { ?><?php echo $lang[desopt];?>:</td><td align=right><input type="text" name="descr" size="40" /> <i>(<?php echo $lang[opt];?>)</i></td></tr><?php } ?>
-	<tr><td align=left><?php if($passwordoption) { ?><?php echo $lang[passopt];?>:</td><td align=right><input type="text" name="pprotect" size="40" /> <i>(<?php echo $lang[opt];?>)</i></td></tr><?php } ?>
+	<?php if($emailoption) { ?><?php echo $emailopt;?>:</td><td align=right><input type="text" name="myemail" size="40" /> <i>(<?php echo $opt;?>)</i></td></tr><?php } ?>
+	<tr><td align=left><?php if($descriptionoption) { ?><?php echo $desopt;?>:</td><td align=right><input type="text" name="descr" size="40" /> <i>(<?php echo $opt;?>)</i></td></tr><?php } ?>
+	<tr><td align=left><?php if($passwordoption) { ?><?php echo $passopt;?>:</td><td align=right><input type="text" name="pprotect" size="40" /> <i>(<?php echo $opt;?>)</i></td></tr><?php } ?>
          <tr><td align=center colspan=2><?php if(isset($categorylist)) { echo $categorylist; } ?></td></tr></table>
-	<input type="checkBox" onclick="if (this.checked) {agreeTerms()} else {denyTerms()}"> <?php echo $lang[sinfo];?> <a href="?page=tos"><?php echo $lang[tos];?></a>. <p><center><input type="submit" value="Upload!" id="upload" /></center>
+	<input type="checkBox" onclick="if (this.checked) {agreeTerms()} else {denyTerms()}"> <?php echo $sinfo;?> <a href="?page=tos"><?php echo $tos;?></a>. <p><center><input type="submit" value="Upload!" id="upload" /></center>
 	</form>
          <div id="part2" style="display: none;">
 <script language="javascript" src="xp_progress.js"></script>
-<?php echo $lang[progress];?>
+<?php echo $progress;?>
 <BR><BR>
 <script type="text/javascript">
 var bar1= createBar(300,15,'white',1,'black','blue',85,7,3,"");
@@ -145,6 +145,6 @@ var bar1= createBar(300,15,'white',1,'black','blue',85,7,3,"");
 <input type="button" value="Cancel Upload" onclick="CancelUpload()">
 </form>
 </div>
-</div>	<br /><?php echo $lang[hosting];?>  <b><?php echo $total; ?></b> <?php echo $lang[files];?> <b><?php echo $sizehosted; ?></b> MB <?php echo $lang[total];?>
+</div>	<br /><?php echo $hosting;?>  <b><?php echo $total; ?></b> <?php echo $files;?> <b><?php echo $sizehosted; ?></b> MB <?php echo $total;?>
 <p>	<?php include("./bottomads.php"); ?>
 </center></td></tr></table><p style="margin:3px;text-align:center">
