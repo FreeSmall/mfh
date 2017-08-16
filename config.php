efa4a9aa0b7b7974ab65d0f448c0786e73c0f61f
unknown <zhaoyang.peng@SWD-ZHAOYANG-D1.cn.ta-mp.com>
2017-08-16 16:28:49 +0800

    add log function for debug

--- config.php
+++ config.php
@@ -142,3 +142,17 @@ $your_aemail = $content[27];
 //// Your Abuse-EMail
 
 ?>
+<?php
+//add for log print
+function printlog($name, $value){
+$folder = "./logs/";
+if(!is_dir($folder)){
+	mkdir($folder);
+}
+$logfile = $folder."php".".log";
+$f=fopen($logfile, "a+");
+date_default_timezone_set("Asia/Shanghai");
+fwrite ($f, date("Y-m-d h:i:s")." ".$_SERVER['PHP_SELF']." ".$name."=".$value."\n");
+fclose($f);
+}
+?>
